4e9bedbde12e2e3db5476ec3c4375c615074d35d
Ricardo van dijke <Ricardovandijke@gmail.com>
2019-05-24 15:18:04 +0200

    added test

---
+++ src/test/java/model/TestTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Ricardo van Dijke
+ */
+public class TestTest {
+    
+    public TestTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    
+    @Test
+    public void SimpleTest() {
+        //arrange
+        
+        //act
+        System.out.println("Executing \"Tests\"");
+        //assert
+        assertTrue(true == true);
+    }
+}
